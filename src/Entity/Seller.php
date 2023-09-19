52ec4e96e9e1b5f1917b3f00e9d5fe7df334905e
Benvalda <107179283+Benvalda@users.noreply.github.com>
2023-09-19 14:08:37 +0200

    lol

---
+++ src/Entity/Seller.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SellerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SellerRepository::class)]
+class Seller
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    private ?int $duration = null;
+
+    #[ORM\OneToOne(mappedBy: 'seller', cascade: ['persist', 'remove'])]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        // unset the owning side of the relation if necessary
+        if ($user === null && $this->user !== null) {
+            $this->user->setSeller(null);
+        }
+
+        // set the owning side of the relation if necessary
+        if ($user !== null && $user->getSeller() !== $this) {
+            $user->setSeller($this);
+        }
+
+        $this->user = $user;
+
+        return $this;
+    }
+}
